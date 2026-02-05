9ebaa26b72e0a44cb688e775ccf8de498caf8abf
mosleym <mosleym@iu.edu>
2026-02-04 23:24:10 -0500

    added docstring for fibonacci function

--- apputil.py
+++ apputil.py
@@ -3,10 +3,22 @@ import pandas as pd
 
 
 def fibonacci(n):
+    """ Finds the nth element in the Fibonacci sequence using recursion.
+
+    Args:
+        n (int): the index of the element
+
+    Returns:
+        int: the nth element in the sequence
+    """
+
+    # The base cases are the first two elements 0, 1
     if n == 0:
         return 0
     elif n == 1:
         return 1
+
+    # The nth element is found by adding the previous two elements
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
     
@@ -20,4 +32,3 @@ def to_binary(n):
 
         else:
             bin_string += '0'
-
